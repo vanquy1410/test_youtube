cf0c8a4f9553c3b0eecea3ca2f7f0c5471bc8047
vanquy1410 <quynguyenpro1410@gmail.com>
2024-07-03 15:55:27 +0700

    fix

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -22,7 +22,7 @@ export const connect = async () => {
   cached.promise =
     cached.promise ||
     mongoose.connect(MONGODB_URL, {
-      dbName: "evently",
+      dbName: "event",
       bufferCommands: false,
       connectTimeoutMS: 30000,
     });
